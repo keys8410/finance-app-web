42ce6da839a107a96439fb88a944e695b56bd706
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-02-28 03:51:07 -0300

    feat: adição de layout compartilhado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
 
+import { Composition, useMediaQuery } from 'atomic-layout';
+import { CompositionContainer, ContainerPages } from './styles';
+import Sidebar from '../components/Sidebar';
+
+const areasMobile = `
+sidebar 
+app 
+`;
+
+const areasDesktop = `
+sidebar titulo usuario
+sidebar app app
+sidebar app app
+`;
 const Pages = () => {
-  return <div>Pages</div>;
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  return (
+    <ContainerPages isMobile={isMobile}>
+      <Composition
+        template={areasMobile}
+        templateMd={areasDesktop}
+        gap={10}
+        gapMd={20}
+        templateCols="1fr"
+        templateColsMd="auto 1fr auto"
+        maxWidth="98%"
+        maxWidthMd="90%"
+        minWidthMd="90%"
+        as={CompositionContainer}
+      >
+        {(Areas) => (
+          <>
+            <Areas.Sidebar>
+              <Sidebar />
+            </Areas.Sidebar>
+            <Areas.Titulo>Titulo</Areas.Titulo>
+            <Areas.Usuario>Usuario</Areas.Usuario>
+            <Areas.App>App</Areas.App>
+          </>
+        )}
+      </Composition>
+    </ContainerPages>
+  );
 };
 
 export default Pages;
+
+/**
+ *  
+    areas={areasMobile}
+    templateMd={areasDesktop}
+    gap={22}
+    gapMd={0}
+    templateCols="1fr"
+    templateRows="auto 1fr"
+    templateColsMd="1.25fr 4fr"
+    templateColsLg="1.25fr 4fr"
+    templateRowsMd="minmax(100px, 1fr)"
+    templateRowsLg="minmax(100px, 1fr)"
+    padding={0}
+    minWidth="95%"
+    maxWidth="98%"
+    minHeight="43rem"
+    maxWidthMd="90%"
+    minWidthMd="90%"
+ */
---
+++ src/pages/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { AppTheme } from '../@types/theme/AppTheme';
+
+export const ContainerPages = styled.div<{ isMobile: boolean }>`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: ${({ isMobile }) => (isMobile ? 'column' : 'row')};
+
+  @media (max-width: 1680px) {
+    padding: 1rem 0;
+    height: 100%;
+  }
+`;
+
+export const CompositionContainer = styled.div<{ theme: AppTheme }>`
+  border-radius: 2.75rem;
+  border: 0.55rem solid ${({ theme }) => theme.backgroundBlocks};
+  padding: 1.8rem 1.5rem;
+`;
